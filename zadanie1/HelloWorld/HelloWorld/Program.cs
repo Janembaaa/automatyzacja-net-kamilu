dfece9b456a0d4eab366d1c88c96b93ed3767356
kamil.ubysz <kamil.ubysz@intive.com>
2017-11-26 22:14:13 +0100

    TypeExitToCloseApp

---
+++ zadanie1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        private static string input;
+        private static bool result;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj Świecie");
+            Console.Write("Wpisz 'exit' i naciśnij klawisz ENTER aby zakończyć działanie: \r\n");
+            string line;
+            while ((line = Console.ReadLine()) != "exit")
+            {
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
